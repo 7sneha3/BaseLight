7a42569286db52969b27e5dde02a73f953667bc8
Sneha Sakhare <154829884+7sneha3@users.noreply.github.com>
2025-07-01 11:53:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+// window.addEventListener('mousemove', (e) => {
+//     document.body.style.setProperty('--mx', e.clientX - (window.innerWidth / 2));
+//     document.body.style.setProperty('--my', e.clientY - (window.innerHeight / 2));
+// });
+
+function updatePosition(x, y) {
+    document.body.style.setProperty('--mx', x - (window.innerWidth / 2));
+    document.body.style.setProperty('--my', y - (window.innerHeight / 2));
+}
+
+// handle mouse
 window.addEventListener('mousemove', (e) => {
-    document.body.style.setProperty('--mx', e.clientX - (window.innerWidth / 2));
-    document.body.style.setProperty('--my', e.clientY - (window.innerHeight / 2));
+    updatePosition(e.clientX, e.clientY);
+});
+
+// handle touch
+window.addEventListener('touchmove', (e) => {
+    const touch = e.touches[0];
+    updatePosition(touch.clientX, touch.clientY);
 });
